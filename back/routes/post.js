abedaca2f144e120d2372ebed506076122e7f46e
thdud <thgml1444@naver.com>
2024-04-14 14:05:20 +0900

    Add : main post 가져오기 ( backend ) - db에서 where에 조건을 추가하여 해당 data를 가져온다 - where : 카테고리 id를 검색하여 모든 post를 load함,  order로 정렬 설정, limit으로 개수 지정 - 변수에 해당 카테고리의 데이터를 담고 post객체에 반환하였다

--- back/routes/post.js
+++ back/routes/post.js
@@ -4,6 +4,7 @@ const hashtag = require("../models/hashtag");
 const category = require("../models/category");
 const router = express.Router();
 
+// 포스트 작성
 router.post("/", async (req, res, next) => {
   console.log("서버 포스트데이터-", req.body);
   try {
@@ -25,6 +26,29 @@ router.post("/", async (req, res, next) => {
       CategoryId: categoryId,
     });
 
+    res.status(201).json(post);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 포스트 로드
+router.get("/", async (req, res, next) => {
+  try {
+    // db에서 포스트 찾기
+    let studyNote = await Post.findAll({
+      where: { CategoryId: 4 },
+      order: [["createdAt", "DESC"]], // createdAt 열을 기준으로 내림차순 정렬
+      limit: 5, // 최대 5개 레코드 로드
+    });
+    let portfolio = await Post.findAll({
+      where: { CategoryId: 5 },
+      order: [["createdAt", "DESC"]],
+      limit: 2,
+    });
+
+    const post = { studyNote, portfolio };
     res.status(201).json(post);
   } catch (error) {
     console.error(error);
