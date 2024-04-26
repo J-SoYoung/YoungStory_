8426e972476503b58e24e1eca5c3d5ace67e0e50
thdud <thgml1444@naver.com>
2024-04-26 10:17:57 +0900

    Add : 포스트 작성시 이미지 추가
    - 서버로 넘어온 이미지 이름을 DB에 저장 후 post와 연결 ( sequelize 메서드사용 addImage )
    - 카테고리도 추가

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -8,15 +8,30 @@ router.get("/", async (req, res, next) => {
     // db에서 포스트 찾기
     const studyNote = await Post.findAll({
       where: { CategoryId: 4 },
+      include: [
+        { model: Image },
+        {
+          model: User, // 게시글 작성자
+          attributes: ["id", "nickname"],
+        },
+      ],
       order: [["createdAt", "DESC"]], // createdAt 열을 기준으로 내림차순 정렬
       limit: 5, // 최대 5개 레코드 로드
     });
     const portfolio = await Post.findAll({
       where: { CategoryId: 5 },
+      include: [
+        { model: Image },
+        {
+          model: User, // 게시글 작성자
+          attributes: ["id", "nickname"],
+        },
+      ],
       order: [["createdAt", "DESC"]],
       limit: 2,
     });
     const post = { studyNote, portfolio };
+
     return res.status(201).json(post);
   } catch (error) {
     console.error(error);
